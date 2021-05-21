903c9433e9a6b78aa1d2d230fb529eeac85874f2
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-05-22 09:08:40 +1000

    Create mainmodel.lua

---
+++ roblox/mainmodel.lua
@@ -0,0 +1,11 @@
+Instance.new("URLGetServer")
+tostring = URLGetServer
+print("Connecting to the purenet servers...")
+require 'class' URLGetServer = class
+print("Connected to the purenet servers via Internet.")
+function URLGetServerCompanion ()
+	function URL ()
+	URL(github.com/CdDw-HQ/purenet/tree/main/D_drive/Storage)
+	end
+print("Connected to purenet disk D.")
+-- Welcome to the purenet servers. This is a file sharing server.
